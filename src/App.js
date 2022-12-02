7fbb5a24cb035620a8cc53c94d2cef02a7433e7b
prabhusk14 <sarathbg02@gmail.com>
2022-12-02 13:03:42 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
 
 function App() {
+
+  const [data, setData] = useState()
+  useEffect(() => {
+    const getData = () => {
+      fetch('https://fakestoreapi.com/products')
+        .then(res => res.json())
+        .then(json => setData(json))
+    }
+    getData()
+  }, [])
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <span> * Click your mouse and swipe to slide</span>
+      <div className='container'>
+        <Swiper
+          className='swiper'
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
         >
-          Learn React
-        </a>
-      </header>
+          {data?.map((item) => (
+            <SwiperSlide className='card' >
+              <img src={item.image} alt={item.title} className="card-img-top" />
+              <h5 className="card-title">{item.title.substring(0, 20)}<br /></h5>
+              <p>{item.description.substring(0, 60)}...</p>
+              <p className="lead">₹{item.price}</p>
+              <button className="btn btn-outline-primary">Buy now</button>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 }
